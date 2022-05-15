b420f710cc98582c808d360ef44f1c3e2a4676ff
TY <1441212902@qq.com>
2022-05-15 23:11:23 +0800

    env

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+export default (() => {
+  switch (process.env.VUE_APP_CURENV) {
+    case "development":
+      return "http://localhost:8080";
+    case "production":
+      return "http://124.223.196.190:8080";
+  }
+})();
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+switch (process.env.VUE_APP_CURENV) {
+  case "development":
+    console.log("当前运行环境为：开发环境");
+    break;
+  case "production":
+    console.log("当前运行环境为：生产环境");
+    break;
+}
